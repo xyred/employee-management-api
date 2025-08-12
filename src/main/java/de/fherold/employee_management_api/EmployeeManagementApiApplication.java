5d615cb20c3a4ee10fdb0b56249378831b908db1
xyred <felixherold@gmx.de>
2025-08-12 13:01:27 +0200

    initial commit

---
+++ src/main/java/de/fherold/employee_management_api/EmployeeManagementApiApplication.java
@@ -0,0 +1,13 @@
+package de.fherold.employee_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/fherold/employee_management_api/EmployeeManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fherold.employee_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
